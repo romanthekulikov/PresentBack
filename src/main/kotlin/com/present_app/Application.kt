8ae41bedc2a607f0384e42dfa88150555ad20090
Kulikov_Roman <kulrom.otr.rarus@gmail.com>
2024-01-11 19:51:23 +0300

    auth 7

--- src/main/kotlin/com/present_app/Application.kt
+++ src/main/kotlin/com/present_app/Application.kt
@@ -1,14 +1,16 @@
 package com.present_app
 
-import com.present_app.plugins.*
+import com.present_app.plugins.configureRouting
+import com.present_app.plugins.configureSecurity
+import com.present_app.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main() {
 //    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
 //        .start(wait = true)
-    embeddedServer(CIO, port = System.getenv("PORT").toInt(), module = Application::module)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), module = Application::module)
         .start(wait = true)
 }
 
--- src/main/kotlin/com/present_app/repository/ConnectionProvider.kt
+++ src/main/kotlin/com/present_app/repository/ConnectionProvider.kt
@@ -8,16 +8,16 @@ object ConnectionProvider {
 
     fun getInstance(): Connection {
         if (connection == null) {
-//            connection = DriverManager.getConnection(
-//                "jdbc:postgresql://monorail.proxy.rlwy.net:25822/railway",
-//                "postgres",
-//                "3adCFd6ceG2-gBA*2g13*4DB43dC-2eD"
-//            )
             connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5433/present_app_db",
+                "jdbc:postgresql://monorail.proxy.rlwy.net:25822/railway",
                 "postgres",
-                "s1210501139"
+                "3adCFd6ceG2-gBA*2g13*4DB43dC-2eD"
             )
+//            connection = DriverManager.getConnection(
+//                "jdbc:postgresql://localhost:5433/present_app_db",
+//                "postgres",
+//                "s1210501139"
+//            )
         }
         return connection!!
     }
