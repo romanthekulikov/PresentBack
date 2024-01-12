59ea872fd2e69cd95797a2f95890119041296237
Kulikov_Roman <kulrom.otr.rarus@gmail.com>
2024-01-12 16:56:42 +0300

    create game 2

--- src/main/kotlin/com/present_app/Application.kt
+++ src/main/kotlin/com/present_app/Application.kt
@@ -9,10 +9,10 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-//    embeddedServer(Netty, port = System.getenv("PORT").toInt(), module = Application::module)
+//    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
 //        .start(wait = true)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), module = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
